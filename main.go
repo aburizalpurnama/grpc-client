9955e5d6d55b9fa3f0c9e780260efb21d79abfd9
aburizalpurnama <purnamar9@gmail.com>
2023-09-07 14:49:44 +0700

    feat: add API endpoint to call gRPC server

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/aburizalpurnama/grpc-server/proto"
+	"github.com/labstack/echo/v4"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/accounts", func(c echo.Context) error {
+		conn, err := grpc.Dial("localhost:45999", grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			e.Logger.Fatalf("can't connect to grpc server: %v", err)
+		}
+		defer conn.Close()
+
+		client := proto.NewAccountsClient(conn)
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+
+		resp, err := client.SelectAccount(ctx, &proto.SelectAccountRequest{})
+		if err != nil {
+			e.Logger.Fatalf("could't select accounts: %v", err)
+		}
+
+		return c.JSON(http.StatusOK, &resp)
+	})
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("APP_PORT"))))
+}
